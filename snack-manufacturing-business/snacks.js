9042537f55961faad37fd1a2c1dd341d3b56e9ac
Yashwant Rao <raoyashwant024@gmail.com>
2025-09-02 19:50:20 +0530

    a

--- snack-manufacturing-business/snacks.js
+++ snack-manufacturing-business/snacks.js
@@ -1,12 +1,4 @@
-const lenis = new Lenis();
 
-lenis.on("scroll", ScrollTrigger.update);
-
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000);
-});
-
-gsap.ticker.lagSmoothing(0);
 
 gsap.registerPlugin(ScrollTrigger);
 
